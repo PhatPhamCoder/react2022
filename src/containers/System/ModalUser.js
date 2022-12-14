0a238d21ed82b91f3d29037b70000c097e17088b
PhatPhamCoder <phamhoangminhphat@gmail.com>
2022-12-14 15:16:49 +0700

    hoan thanh xoa nguoi dung

---
+++ src/containers/System/ModalUser.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { emitter } from '../../utils/emitter';
+
+class ModalUser extends Component {
+
+
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            address: ''
+        }
+        this.listenToEmiiter();
+    }
+
+    listenToEmiiter() {
+        emitter.on('EVENT_CLEAR_MODAL_DATA', () => {
+            this.setState({
+                email: '',
+                password: '',
+                firstName: '',
+                lastName: '',
+                address: ''
+            })
+        })
+    } //bus event
+
+    componentDidMount() {
+        console.log('mouting modal')
+    }
+
+    toggle = () => {
+        this.props.toggelFromParent();
+    }
+
+    handleOnChangeInput = (event, id) => {
+        //bad code
+        // this.state[id] = event.target.value;
+
+        // this.setState({
+        //     ...this.state
+        // }, () => {
+        //     console.log('check bad state', this.state)
+        // })
+
+        //good code
+        let copyState = { ...this.state };
+        copyState[id] = event.target.value;
+
+        this.setState({
+            ...copyState
+        })
+    }
+
+    checkValidateInput = () => {
+        let isValid = true;
+        let arrInput = ['email', 'password', 'firstName', 'lastName', 'address'];
+        for (let i = 0; i < arrInput.length; i++) {
+            console.log('check inside loop:', this.state[arrInput[i]], arrInput[i])
+            if (!this.state[arrInput[i]]) {
+                isValid = false;
+                alert('Missing parameters: ' + arrInput[i]);
+                break;
+            }
+        }
+        return isValid;
+    }
+
+    handleAddNewuser = () => {
+        let isValid = this.checkValidateInput();
+        if (isValid === true) {
+            this.props.createNewUser(this.state);
+        }
+
+    }
+
+    render() {
+        return (
+            <Modal isOpen={this.props.isOpen}
+                toggle={() => { this.toggle() }}
+                className={'modal-user-container'}
+                size="lg"
+            >
+                <ModalHeader toggle={() => { this.toggle() }}>Create a new user</ModalHeader>
+                <ModalBody>
+                    <div className='modal-user-body'>
+                        <div className='col-6 input-container'>
+                            <label>Email</label>
+                            <input
+                                type='text'
+                                onChange={(event) => { this.handleOnChangeInput(event, "email") }}
+                                value={this.state.email}
+                            ></input>
+                        </div>
+                        <div className='col-6 input-container'>
+                            <label>Password</label>
+                            <input
+                                type='password'
+                                onChange={(event) => { this.handleOnChangeInput(event, "password") }}
+                                value={this.state.password}
+                            ></input>
+                        </div>
+                        <div className='col-6 input-container'>
+                            <label>First Name</label>
+                            <input
+                                type='text'
+                                onChange={(event) => { this.handleOnChangeInput(event, "firstName") }}
+                                value={this.state.firstName}
+                            ></input>
+                        </div>
+                        <div className='col-6 input-container'>
+                            <label>Last Name</label>
+                            <input
+                                type='text'
+                                onChange={(event) => { this.handleOnChangeInput(event, "lastName") }}
+                                value={this.state.lastName}
+                            ></input>
+                        </div>
+                        <div className='col-6 input-container max-width-input'>
+                            <label>Address</label>
+                            <input
+                                type='text'
+                                onChange={(event) => { this.handleOnChangeInput(event, "address") }}
+                                value={this.state.address}
+                            ></input>
+                        </div>
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <Button
+                        color="primary"
+                        className='px-3'
+                        onClick={() => { this.handleAddNewuser() }}
+                    >Add New</Button>{' '}
+                    <Button color="secondary" className='px-3' onClick={() => { this.toggle() }}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalUser);
+
+
+
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import { getAllUsers } from '../../services/userService';
+import { getAllUsers, createNewUserService, deleteUserService } from '../../services/userService';
+import ModalUser from './ModalUser';
+import { emitter } from '../../utils/emitter';
+
 class UserManage extends Component {
 
-    constructor(prop) {
-        super(prop);
+    constructor(props) {
+        super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
     async componentDidMount() {
+        await this.getAllUserFromReact();
+    }
+
+    getAllUserFromReact = async () => {
         let response = await getAllUsers('ALL');
         if (response && response.errCode === 0) {
             this.setState({
@@ -21,44 +29,103 @@ class UserManage extends Component {
         }
     }
 
-
-    render() {
-        console.log('check render', this.state)
-        let arrUsers = this.state.arrUsers;
-        return (
-            <div className="users-container">
-                <div className='col- 12 title'>manage users with PhatPham</div>
-                <div className='user-table mt-4 mx-1'>
-                    <table id="customers">
-                        <tr>
-                            <th>Email</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Address</th>
-                            <th>Actions</th>
-                        </tr>
-                        {arrUsers && arrUsers.map((item, index) => {
-                            console.log('PhatPham check map', item, index)
-                            return (
-                                <tr>
-                                    <td>{item.email}</td>
-                                    <td>{item.firstName}</td>
-                                    <td>{item.lastName}</td>
-                                    <td>{item.address}</td>
-                                    <td>
-                                        <button className='btn-edit'><i class="fas fa-pencil-alt"></i></button>
-                                        <button className='btn-delete'><i class="fas fa-trash"></i></button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                        }
-                    </table>
-                </div>
-            </div>
-        );
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
     }
 
+    toggelUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
+
+
+    createNewUser = async (data) => {
+        try {
+            let response = await createNewUserService(data);
+            if (response && response.errCode !== 0) {
+                alert(response.errMessage)
+            } else {
+                await this.getAllUserFromReact();
+                this.setState({
+                    isOpenModalUser: false
+                })
+
+                emitter.emit('EVENT_CLEAR_MODAL_DATA')
+            };
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+
+    handleDeleteUser = async (user) => {
+        console.log('click delete', user)
+        try {
+            let res = await deleteUserService(user.id)
+            if (res && res.errCode === 0) {
+                await this.getAllUserFromReact();
+            }
+            else {
+                alert(res.errMessage)
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    render() {
+        let arrUsers = this.state.arrUsers;
+        console.log(arrUsers);
+        return (
+            <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggelFromParent={this.toggelUserModal}
+                    createNewUser={this.createNewUser}
+                />
+                <div className='col- 12 title'>Manage users with PhatPham</div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3'
+                        onClick={() => this.handleAddNewUser()}
+                    ><i className="fas fa-plus"></i> Add new users
+                    </button>
+                </div>
+                <div className='user-table mt-4 mx-1'>
+                    <table id="customers">
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Address</th>
+                                <th>Actions</th>
+                            </tr>
+                            {arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                            <button
+                                                className='btn-delete'
+                                                onClick={() => this.handleDeleteUser(item)}
+                                            ><i className="fas fa-trash"></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div >
+        );
+    }
 }
 
 const mapStateToProps = state => {
